4c95a717ab633928b9cb0cbe18d4810d35c59f48
Alan McIvor <20034584@myclyde.ac.uk>
2023-09-08 15:16:02 +0100

    updated

--- sessionTwo/partials/header.php
+++ sessionTwo/partials/header.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= Root_directory?>style.css">    
+    <link rel="stylesheet" href="<?= Root_Directory ?>style.css">    
     <title><?= $pageTitle ?></title>
 
 </head>
